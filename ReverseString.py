26de5f29a782e41c4a215b74c4e54fee7a0fbd82
Aliza05 <aliza.zia524@gmail.com>
2022-11-12 13:32:02 +0500

    New files of python functions are added

---
+++ ReverseString.py
@@ -0,0 +1,5 @@
+def string_reverse(user_string):
+    return user_string[::-1]
+
+input_string = input("Enter a string: ")
+print("Reversed String is: " + string_reverse(input_string))
---
+++ UpperLowerCaseCount.py
@@ -0,0 +1,18 @@
+def string_upper_lower_count(user_string):
+    upper_case = 0
+    lower_case = 0
+
+    for characters in user_string:
+        if characters.isupper():
+            upper_case += 1
+        elif characters.islower():
+            lower_case += 1
+        else:
+            pass
+
+    print("The user entered string is: " + user_string)
+    print("The number of upper case letters in string are: " + str(upper_case))
+    print("The number of lower case letters in string are: " + str(lower_case))
+
+input_string = input("Enter the string: ")
+string_upper_lower_count(input_string)
